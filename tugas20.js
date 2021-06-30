a7f7c15291bcf9442b86d5e1675a21067602f1b0
oktyajipangestu <86592837+oktyajipangestu@users.noreply.github.com>
2021-06-30 21:43:14 +0700

    Add files via upload

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function mat() {
+    let text1 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+    console.log(text1.match("are"));
+    console.log(text1.match("words"));
+}
+
+mat();
